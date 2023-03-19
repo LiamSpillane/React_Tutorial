9d30cd259796ce9ffb2ad5dbcab94a0b5a7c6d3c
Liam Spillane <123liamspillane@gmail.com>
2023-03-19 03:20:47 -0400

    parts 21-32

--- dojo-blog/src/useFetch.js
+++ dojo-blog/src/useFetch.js
@@ -6,24 +6,30 @@ const useFetch = (url) => {
     const [error, setError] = useState(null)
 
     useEffect(() => {
-        setTimeout(() => {
-            fetch(url)
-            .then(res => {
-                if(!res.ok) {
-                    throw Error('could not fetch the data for that resource')
-                }
-                return res.json()
-            })
-            .then(data => {
-                setData(data)
-                setIsPending(false)
-                setError(null)
-            })
-            .catch(err => {
+        const abortCont = new AbortController()
+
+        fetch(url, { signal: abortCont.signal })
+        .then(res => {
+            if(!res.ok) {
+                throw Error('could not fetch the data for that resource')
+            }
+            return res.json()
+        })
+        .then(data => {
+            setData(data)
+            setIsPending(false)
+            setError(null)
+        })
+        .catch(err => {
+            if (err.name === 'AbortError') {
+                console.log('fetch aborted')
+            } else {
                 setIsPending(false)
                 setError(err.message)
-            })
-        }, 1000);
+            }
+        })
+
+        return () => abortCont.abort()
     }, [url])
 
     return { data, isPending, error }
